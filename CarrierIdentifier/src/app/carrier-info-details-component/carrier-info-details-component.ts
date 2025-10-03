34019da3a8c42bef61dc2c253c597c037364f059
DASKALELIS Emmanouil <DaskalelisE@unisystems.gr>
2025-10-03 14:36:33 +0300

    BehaviorSubject added

--- CarrierIdentifier/src/app/carrier-info-details-component/carrier-info-details-component.ts
+++ CarrierIdentifier/src/app/carrier-info-details-component/carrier-info-details-component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { CarrierService } from '../shared/services/carrier-service';
+import { pipe } from 'rxjs';
 
 @Component({
   selector: 'app-carrier-info-details-component',
@@ -6,6 +8,13 @@ import { Component } from '@angular/core';
   templateUrl: './carrier-info-details-component.html',
   styleUrl: './carrier-info-details-component.css'
 })
-export class CarrierInfoDetailsComponent {
-
+export class CarrierInfoDetailsComponent implements OnInit {
+  private carrierService = inject(CarrierService);
+  carrierName: string ="";
+  
+  ngOnInit(): void {
+    this.carrierService.carrier$.subscribe(pipe(name => {
+      this.carrierName = name;
+    }));
+  }
 }
--- CarrierIdentifier/src/app/shared/services/carrier-service.ts
+++ CarrierIdentifier/src/app/shared/services/carrier-service.ts
@@ -1,20 +1,22 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 import carriers from '../../shared/carrierinfo.json';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CarrierService {
+  private carrierSubject= new BehaviorSubject<string>('Unknown');
+  carrier$ = this.carrierSubject.asObservable();
 
   getCarrierInfo(phoneNumber: string) {
-    console.log(carriers.carriers);
-    
     const carrier = carriers.carriers.find(cp => cp.phone.substring(0, 3) === phoneNumber.substring(0, 3));
 
     if (carrier) {
+      this.carrierSubject.next(carrier.name);
       return carrier.name;
     }
-
+    this.carrierSubject.next('Unknown');
     return 'Unknown';
   }
 }
